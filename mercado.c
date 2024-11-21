96d6694b6692b98b848fdf1bf93dc7eae4b86378
Liz Martins <158207019+lizmartns@users.noreply.github.com>
2024-11-21 09:32:14 -0300

    Update mercado.c

--- mercado.c
+++ mercado.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+int cadastrarProduto(Produto produto[], int tamanhoProduto){
+
+    for(int i=0; i>tamanhoProduto; i++){
+        prinft("Cadastrar produto");
+        printf("Digite o código do produto: ");
+        scanf("%d", &produto[i].codigo);
+
+        printf("Digite o nome do produto: ");
+        scanf("%s", &produto[i].nome);
+        
+        printf("Digite o nome do produto: ");
+        scanf("%f", &produto[i].preco);
+        
+    }
+    return produtosCadastrados + 1;
+}
+
 int main(){
     return 0;
 }
