3d49a44a75d39830bedc59652289b517d4664338
Liz Martins <158207019+lizmartns@users.noreply.github.com>
2024-11-21 09:47:33 -0300

    Update mercado.c

--- mercado.c
+++ mercado.c
@@ -18,5 +18,42 @@ int cadastrarProduto(Produto produto[], int tamanhoProduto){
 }
 
 int main(){
+
+    int tamanhoProduto =50;
+    int opcao;
+    do {
+        printf("Menu\n");
+        printf("1.Cadastrar Produto.\n");
+        printf("2.Listar Produtos.\n");
+        printf("3.Comprar Produto\n");
+        printf("4.Visualizar Carrinho.\n");
+        printf("5.Fechar Pedido.\n");
+        printf("6.Sair do Sistema.\n");
+        scanf("%d", opcao);
+
+        switch(opcao)
+        case 1: 
+            cadastrarProduto();
+            break;
+        case 2:
+            listarProduto();
+            break;
+        case 3 :
+            comprarProduto();
+            break;
+        case 4 :
+            visualizarCarrinho();
+            break;
+        case 5 :
+            fecharPedido();
+            break;
+        case 6 :
+            printf("O sistema está sendo encerrado !");
+        default:
+        printf("Opção inválida! Por favor, tente novamente.\n");
+
+    }while(opcao != 6);
+    
+
     return 0;
 }
